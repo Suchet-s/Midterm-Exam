fe77270394ee05d9bc7dd0068f031d673b4b65cb
Suchet-s <suchet-s@rmutp.ac.th>
2025-01-10 01:26:09 +0700

    ีupp

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function showExercises() {
-    const exercises = [
-        "1. สร้าง Array ที่มีตัวเลข 10, 20, 30, 40 และ 50 และแสดงผลตัวเลขที่ตำแหน่งที่ 2",
-        "2. เพิ่มค่า 60 เข้าไปใน Array และแสดงผล Array ทั้งหมด"
-    ];
-
-    alert("แบบฝึกหัด:\n\n" + exercises.join("\n\n"));
-}
